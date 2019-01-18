adf3454d5de42ae416696e9be191bc7b0c81c40e
Jakob Spirk <jakob.spirk@gmx.net>
2019-01-18 10:57:59 +0100

    santa

---
+++ MegaDungeonFighterUnity/Assets/Scripts/SantaController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SantaController : MonoBehaviour
+{
+
+    public Animator animator;
+    protected SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var y= CrossPlatformInputManager.GetAxis("Vertical");
+        var x = CrossPlatformInputManager.GetAxis("Horizontal");
+        if (x < 0) {
+            spriteRenderer.flipX;
+        }
+        transform.Translate(new Vector3(x*0.1f,y*0.1f,0));
+        
+    }
+}
