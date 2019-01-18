50b519e6050f42e0621a70fed6b739704c3b74a9
chuckborris89 <boris.fuchs89@gmail.com>
2019-01-18 10:11:09 +0100

    update room2 + enemy movment

---
+++ MegaDungeonFighterUnity/Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+
+    private Transform target;
+    public float speed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector3 pos = transform.position;
+
+        if (pos.x-target.position.x < 5 && pos.x - target.position.x > -5)
+        {
+            if (pos.y - target.position.y < 5 && pos.y - target.position.y > -5)
+            {
+                pos.y += speed * Time.deltaTime;
+                pos.y -= speed * Time.deltaTime;
+                pos.x += speed * Time.deltaTime;
+                pos.x -= speed * Time.deltaTime;
+
+            }
+
+        }
+
+       
+
+    }
+
+
+
+}
